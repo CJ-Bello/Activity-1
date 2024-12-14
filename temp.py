6a40212bb7232be798b529bd741db89f077c4b3d
CJ-Bello <carla.bello.college@ustangelicum.edu.ph>
2024-12-14 14:22:36 +0800

    Temperature

---
+++ temp.py
@@ -0,0 +1,5 @@
+celsius = float(input("Enter the Temperature in Celsius:"))
+print(f"{celsius} Fahrenheit to Celsius: {(1.8 * celsius) + 32} degrees fahrenheit")
+
+fahrenheit = float(input("Enter the Temperature in Fahrenheit:"))
+print(f"{fahrenheit} Celsius to Fahrenheit: {0.56 * (fahrenheit - 32)} degrees celsius")
